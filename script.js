527de969b4f8436ee857954a535e5a409db058fc
Teslim <104563512+tesvstheworld@users.noreply.github.com>
2024-07-31 14:19:59 +0100

    icons added to dropdowns

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
+  const hamburger = document.querySelector('.hamburger');
+  const navbarMenu = document.querySelector('.navbar-menu');
+  const navbarActions = document.querySelector('.navbar-actions');
+
+  hamburger.addEventListener('click', function () {
+    navbarMenu.classList.toggle('active');
+    navbarActions.classList.toggle('active');
+  });
+});
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
   var dropdownToggles = document.querySelectorAll('.dropdown-toggle')
 
   dropdownToggles.forEach(function (toggle) {
